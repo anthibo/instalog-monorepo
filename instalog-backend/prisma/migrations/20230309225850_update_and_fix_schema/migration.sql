62af90ef52be1fca933ea4add44fde6a3b90a536
procrew <anthibosama@gmail.com>
2023-03-10 01:00:31 +0200

    BE/wip: update and fix schema

--- instalog-backend/prisma/migrations/20230309225850_update_and_fix_schema/migration.sql
+++ instalog-backend/prisma/migrations/20230309225850_update_and_fix_schema/migration.sql
@@ -1,17 +1,16 @@
 -- CreateTable
 CREATE TABLE `Event` (
     `id` VARCHAR(191) NOT NULL,
-    `email` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `group` VARCHAR(191) NOT NULL,
     `actor_id` VARCHAR(191) NOT NULL,
     `action_id` VARCHAR(191) NOT NULL,
+    `target_id` VARCHAR(191) NOT NULL,
+    `target_name` VARCHAR(191) NOT NULL,
     `location` VARCHAR(191) NOT NULL,
     `occurred_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `metadata` JSON NULL,
-    `actionId` VARCHAR(191) NOT NULL,
 
-    UNIQUE INDEX `Event_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -19,6 +18,8 @@ CREATE TABLE `Event` (
 CREATE TABLE `Actor` (
     `id` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `hashed_password` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -28,6 +29,7 @@ CREATE TABLE `Action` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
 
+    UNIQUE INDEX `Action_name_key`(`name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -35,4 +37,4 @@ CREATE TABLE `Action` (
 ALTER TABLE `Event` ADD CONSTRAINT `Event_actor_id_fkey` FOREIGN KEY (`actor_id`) REFERENCES `Actor`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Event` ADD CONSTRAINT `Event_actionId_fkey` FOREIGN KEY (`actionId`) REFERENCES `Action`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Event` ADD CONSTRAINT `Event_action_id_fkey` FOREIGN KEY (`action_id`) REFERENCES `Action`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
