2094fe775408da20c490f284f7744c655e7f7ed4
procrew <anthibosama@gmail.com>
2023-03-15 04:31:41 +0200

    BE/fix: add dynamic port mapping

--- instalog-backend/src/main.ts
+++ instalog-backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('/api');
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log('app started on port 3000');
 }
 bootstrap();
